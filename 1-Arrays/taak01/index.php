7ac8793a2bfad1ecea00f04be419858461222206
Samuel Adu <samuel.adu@student.rocva.nl>
2021-04-22 12:05:53 +0200

    array

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,5 @@
+<?php
+$student = array("voornaam" =>"Muhammad","achternaam" => "Ali", "leeftijd" => 17, "klas"=> "9G");
+
+echo "Gegevens Muhammad: Voornaam: ". $student["voornaam"]. " Achternaam: " .$student["achternaam"]. " Leeftijd: ".$student["leeftijd"]. " Klas: ".$student["klas"];
+?>
---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,7 @@
+<?php
+$persoon = array("voornaam" =>"Willem", "achternaam" => "Alexander", "leeftijd" => 53, "opleiding" => "Software Ontwikkeling");
+
+
+ echo $persoon['voornaam'] ." ". $persoon['achternaam'] ." ". "van Oranje is " . $persoon['leeftijd'] . " " . "jaar oud "." ". "en is student aan het ROC van Amsterdam aan de opleiding"." ". $persoon['opleiding'];
+
+?>
