658cea65d4a33bafb3bad4131cea23feaa673cda
Samuel Adu <samuel.adu@student.rocva.nl>
2021-04-23 11:24:08 +0200

    for

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,6 @@
+<?php
+for($x = 0; $x < 18; $x++){
+    echo "<ul> <li>Ik ben $x jaar, dus ik mag nog niet stemmen </ul> </li>";
+}
+echo " <ul> <li>Ik ben 18 jaar oud dus ik heb stemrecht! </ul> </li>"; 
+?>
