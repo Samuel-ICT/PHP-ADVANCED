a60e461ffebbd66d902dd2e4548f60d0078cef05
Samuel Adu <samuel.adu@student.rocva.nl>
2021-04-27 18:47:02 +0200

    formulier

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style = "text-align:center; padding-top: 150px; font-size: 50px" >
+<?php
+echo $_POST["voornaam"]." "." ";
+echo $_POST["achternaam"];
+echo "<br>";
+echo "Klas:"." " .$_POST["klas"];
+echo "<br>";
+echo $_POST["leeftijd"]." ". "jaar";
+echo "<br>";
+echo $_POST["adres"];
+echo "<br>";
+echo $_POST["woonplaats"];
+echo "<br>";
+echo $_POST["muziekband"];
+?>
+</body>
+</html>
