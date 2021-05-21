9ae9be5c803c41ad818d1c380b3f26cacab258c1
Samuel Adu <samuel.adu@student.rocva.nl>
2021-05-21 12:35:31 +0200

    wijziging eindopdracht

--- opdracht/advanced-1.php
+++ opdracht/advanced-1.php
@@ -5,36 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+	<style>
+		.form-group{
+			margin-top: 24px;
+		}
+	</style>
 </head>
 <body>
-  <form name = "Form"  action = "advanced-2.php" method = "POST">
-  Tabel-border dikte (px): <input type = "text" name = "border">
-  <br> <br>
-  Cel-padding (px): <input type = "text" name = "padding">
-  <br> <br>
-  <label for="background">Achtergrondkleur:</label>
-  <select name="background" >
-      <?php
-        $kleuren = array("red", "blue", "green", "black", "brown");
-        foreach ($kleuren as $achtergroundkleur) {
-           echo "<option> $achtergroundkleur </option>";
-        }
-        
-        ?>
-      </select>
-      <br> <br>
-    <label for="tekstkleuren">Tekstkleur:</label>
-    <select name="tekstkleuren" >
-      <?php
-        $kleuren = array("red", "blue", "green", "black", "brown");
-        foreach ($kleuren as $tekstkleur) {
-           echo "<option> $tekstkleur </option>";
-        }
-        
-        ?>
-      </select>
-    <br> <br>
-    <input type = "submit" value = "Verzend">
+  	<form name="Form"  action = "advanced-2.php" method = "POST">
+		<div class="form-group">
+			<label for="border">Tabel-border dikte (px): </label>
+			<input type = "text" name="border" id="border">
+		</div>
+    	<div class="form-group">
+			<label for="padding">Cel-padding (px): </label>
+			<input type = "text" name = "padding" id="padding">
+		</div>
+		<div class="form-group">
+			<label for="background">Achtergrondkleur:</label>
+			<select name="background" id="background">
+				<?php
+				$kleuren = array("red", "blue", "green", "black", "brown");
+				foreach ($kleuren as $achtergroundkleur) {
+					echo "<option> $achtergroundkleur </option>";
+				}
+				
+				?>
+			</select>
+		</div>
+    	<br> <br>
+    	
+        <br> <br>
+      	<label for="tekstkleuren">Tekstkleur:</label>
+      	<select name="tekstkleuren" >
+			<?php
+		
+			foreach ($kleuren as $tekstkleur) {
+				echo "<option> $tekstkleur </option>";
+			}
+			
+			?>
+        </select>
+      	<br> <br>
+      	<input type = "submit" value = "Verzend">
   </form>
 </body>
 </html>
